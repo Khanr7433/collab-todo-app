623c9195da48963abf50940d485080d648e819d2
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-07-05 12:26:46 +0530

    Add authentication middleware and response and error handling utilities

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -11,7 +12,12 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
-app.use(express.json());
+app.use(cookieParser());
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies.token || req.headers.authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    return new ApiError(401, "Not authorized, no token");
+  }
+
+  if (token === "null") {
+    return new ApiError(401, "Not authorized, invalid token");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return new ApiError(401, "Not authorized, token failed");
+    }
+
+    const user = await User.findById(decoded._id).select("-password");
+
+    if (!user) {
+      return new ApiError(401, "Not authorized, user not found");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return new ApiError(401, "Not authorized, token failed", error);
+  }
+});
+
+export default authMiddleware;
