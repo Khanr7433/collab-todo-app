10aa5066589b5e1851134e0481c5ba7c53a4d2b6
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-08-30 18:17:45 +0530

    feat(backend): add project reference to Task model

--- backend/src/models/tasks.model.js
+++ backend/src/models/tasks.model.js
@@ -33,6 +33,10 @@ const taskSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+    },
   },
   {
     timestamps: true,
