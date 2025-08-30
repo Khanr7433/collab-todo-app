ec255af6ab4326a91006b4318e096e1fad54c91f
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-08-30 18:18:03 +0530

    feat(backend): add createProject controller function

---
+++ backend/src/controllers/projects.controller.js
@@ -0,0 +1,61 @@
+import { Project } from "../models/projects.model.js";
+import { Task } from "../models/tasks.model.js";
+import { ActionLog } from "../models/actionLog.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+
+const createProject = asyncHandler(async (req, res) => {
+  const { name, description, members } = req.body;
+
+  if (!name) {
+    throw new ApiError(400, "Project name is required");
+  }
+
+  const existingProject = await Project.findOne({ name });
+
+  if (existingProject) {
+    throw new ApiError(400, "Project with this name already exists");
+  }
+
+  const owner = req.user._id;
+
+  if (!owner) {
+    throw new ApiError(400, "User not authenticated");
+  }
+
+  const project = await Project.create({
+    name,
+    description,
+    owner,
+    members: members || [],
+  });
+
+  if (!project) {
+    throw new ApiError(500, "Something went wrong while creating project");
+  }
+
+  const createdProject = await Project.findById(project._id)
+    .populate("owner", "fullName email")
+    .populate("members", "fullName email");
+
+  if (!createdProject) {
+    throw new ApiError(500, "Something went wrong while fetching created project");
+  }
+
+  const actionLog = await ActionLog.create({
+    action: "Project Created",
+    user: owner,
+    project: project._id,
+  });
+
+  if (!actionLog) {
+    throw new ApiError(500, "Failed to create action log for project creation");
+  }
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, { project: createdProject }, "Project created successfully"));
+});
+
+export { createProject };
