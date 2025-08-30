7b3632fb381ebda2d4873a6c4837182bdce41c1e
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-08-30 18:28:29 +0530

    feat(frontend): export Projects page from index

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,5 +4,6 @@ import Login from "./Login";
 import KanbanBoard from "./KanbanBoard";
 import ActionLog from "./ActionLog";
 import Logout from "./Logout.jsx";
+import Projects from "./Projects";
 
-export { Home, Register, Login, KanbanBoard, ActionLog, Logout };
+export { Home, Register, Login, KanbanBoard, ActionLog, Logout, Projects };
