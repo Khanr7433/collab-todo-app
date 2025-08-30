fcf69d03c24842a58291fc7606dce0d8d0f5762e
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-08-30 18:28:03 +0530

    feat(frontend): add Projects page with ProjectsList and OnlineUsers

---
+++ frontend/src/pages/Projects.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ProjectsList, OnlineUsers } from "../components";
+import useSocket from "../hooks/useSocket";
+import { useAuth } from "../context/AuthContext";
+
+const Projects = () => {
+  const { user } = useAuth();
+  const { onlineUsers } = useSocket(() => {}, user);
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+          {/* Main Projects Content */}
+          <div className="lg:col-span-3">
+            <ProjectsList />
+          </div>
+          
+          {/* Sidebar */}
+          <div className="lg:col-span-1">
+            <div className="sticky top-8">
+              <OnlineUsers onlineUsers={onlineUsers} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
